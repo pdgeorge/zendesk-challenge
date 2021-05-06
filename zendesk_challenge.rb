c4c1cdfceec46f3beed8a135f049f5e3bbbedeb1
Pdgeorge <pdgeorge.geekpride@gmail.com>
2021-05-06 23:20:04 +1000

    efficiency update
    While adding more information, the previous method of displaying information showed at scale would become less viable. Updated to a slightly more efficient method
    
    Adding notes.txt to include notes taken regarding full json information received with each ticket. All tickets added via bulk method were priority nil, however first two tickets had a priority.

--- zendesk_challenge.rb
+++ zendesk_challenge.rb
@@ -9,12 +9,22 @@ def read_tickets()
   uri = URI(url)
 
   request = Net::HTTP::Get.new(uri)
-  request.basic_auth 'pdgeorge.geekpride@gmail.com', 'PASSWORD_GOES_HERE'
+  request.basic_auth 'pdgeorge.geekpride@gmail.com', 'Zendeskwolf236'
 
   response = Net::HTTP.start(uri.hostname, uri.port, :use_ssl => uri.scheme == "https", :verify_mode => OpenSSL::SSL::VERIFY_NONE) {|http|
     http.request(request)
   }
   return response
+end
+
+def pager(x)
+  if(x % 25 == 0 && x != 0)
+    puts("================================================================================")
+    puts("Press Enter To Get The Next Page Of 25")
+    puts("================================================================================")
+    STDOUT.flush
+    gets()
+  end
 end
 
 def display_all(all)
@@ -23,13 +33,7 @@ def display_all(all)
   for i in 0...all["tickets"].length
       puts("The ID of the #{i}th item is: #{all["tickets"][i]["id"]}")
       puts("The Subject of the #{i}th item is: #{all["tickets"][i]["subject"]}")
-    if(i%25 == 0 && i != 0)
-      puts("================================================================================")
-      puts("Press Enter To Get The Next Page Of 25")
-      puts("================================================================================")
-      STDOUT.flush
-      gets()
-    end
+    pager(i)
   end
 end
 
@@ -50,7 +54,7 @@ def search_ticket(all)
   end while (!(1..5).member?(choice))
   case choice
   when 1
-    search_string(all, "description")
+    search_string(all, "subject")
   end
 end
 
@@ -68,11 +72,20 @@ def search_string(all, specific)
   puts("Tell me what you want to find please")
   STDOUT.flush
   search = gets.chomp
+  found = 0
+  found_tickets = Array.new()
   for i in 0...all["tickets"].length
-    # puts(all["tickets"][i][specific])
-    puts(all["tickets"][i][specific]) if all["tickets"][i][specific].include? search
-    puts(all["tickets"][i]["subject"]) if all["tickets"][i][specific].include? search
-    puts("================================================================================") if all["tickets"][i][specific].include? search
+    found_ticket = all["tickets"][i] if all["tickets"][i][specific].include? search
+    found_tickets << found_ticket
+  end
+  found_tickets.each do |i|
+    puts("#{search} was found in the #{specific} of the following ticket:")
+    puts("ID is: #{i["id"].to_s}")
+    puts("Subject is: #{i["subject"]}")
+    puts("Priority is: #{i["priority"]}")
+    puts("================================================================================")
+    found += 1
+    pager(found)
   end
 end
 
@@ -83,17 +96,14 @@ def main_menu()
   begin
     puts("Please make a choice between 1 and 3")
     puts("Enter 1 to view all tickets.")
-    puts("Enter 2 to view a specific ticket")
-    puts("Enter 3 to search tickets")
+    puts("Enter 2 to search tickets")
     STDOUT.flush
     choice = gets.chomp().to_i
-  end while (!(1..3).member?(choice))
+  end while (!(1..2).member?(choice))
   case choice
   when 1
     display_all(parsed)
   when 2
-    display_single(parsed)
-  when 3
     search_ticket(parsed)
   else
     puts("How did you get here?")
